dac0ad81384b944bbcb24cbd5fb214fc488dca9d
Burak Akin <burakakin@buraks.local>
2018-08-06 19:16:33 +0300

    Structure was added

--- ParseJsonExample/ViewController.swift
+++ ParseJsonExample/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
+
+struct Album: Decodable {
+    let name: String
+    let artist: Artist
+    let tracks: [Track]
+}
+
+struct Track: Decodable {
+    let duration: Int
+    let name: String
+}
+
+struct Artist: Decodable {
+    let founded: Int
+    let name: String
+    let members: [String]
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      //  var apps = [App]()
+        
+        let url = URL(string: "http://www.json-generator.com/api/json/get/bVFwrzeIHS?indent=2")
+        URLSession.shared.dataTask(with: url!) { (data, response, err) in
+            guard let data = data else { return }
+            do{
+                let appData = try JSONDecoder().decode(Album.self, from: data)
+                print(appData.tracks[0].name)
+                
+              
+                
+            }catch let err{
+                
+                print("Error",err)
+            }
+            
+            
+        }.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
 
+    
+    
+    
 
 }
 
